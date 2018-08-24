781c4268016eb9fc8a1a3e219861373111ba7b86
unknown <cascosole@gmail.com>
2018-08-23 22:22:02 -0300

    cambio en models padron

--- padron/models.py
+++ padron/models.py
@@ -13,44 +13,45 @@ class persona(models.Model):
 		
 		return self.nombre + self.domicilio
 
-class marca(models.Model):
-	nombre_marca=models.CharField(max_length=100)
-	def __str__(self):
-		return self.nombre_marca
-class origen(object):
+class origen(models.Model):
 	"""docstring for origen"""
 	nombre_origen=models.CharField(max_length=10)
-	def __str__(self:
-		
+	def __str__(self):
+
 		return self.nombre_origen
-class fabrica(object):
-	cod_fabrica=models.
+
+class fabrica(models.Model):
+	cod_fabrica=models.IntegerField(max_length=4)
+	id_origen=models.ForeignKey(origen,null=True, blank=True)
 			"""docstring for fabrica"""
 			
 						
-class tipo(object):
+class tipo(models.Model):
 	"""docstring for tipo"""
 	tv=models.CharField(max_length=50)
+	id_fabrica=models.ForeignKey(fabrica)
 	def __str__(self):
 		return self.nombre_tipo
-class descripcion(object):
-	"""docstring for descripcion"""
-	cod_modelo=models.ForeignKey(modelo)
-	desc=models.CharField(max_length=50)
+
+class marca(models.Model):
+	nombre_marca=models.CharField(max_length=100)
+	id_tipo=models.ForeignKey(tipo)
+	def __str__(self):
+		return self.nombre_marca
+	nombre_modelo=models.CharField(max_length50)
+	id_marca=models.ForeignKey(marca)
 		
 
+class descripcion(models.Model):
+	
+	desc=models.CharField(max_length=50)
+	cod_modelo=models.ForeignKey(modelo)	
+
 		
-class auto(models.Model):
-	id_origen=models.IntegerField(max_length=50, blank=True)
-	cod_fabrica=models.IntegerField(null=True, blank=True)
-	cod_marca=models.CharField(max_length=5, blank=True)
-	cod_tipo=models.IntegerField(null=True, blank=True)
-	cod_modelo=models.CharField(max_length=50,null=True, blank=True)
-	tv=models.CharField(max_length=3, blank=True)
-	marca=models.CharField(max_length=50, blank=True)
-	descripcion=models.CharField(max_length=100, blank=True)
-	tipo=models.CharField(max_length=100, blank=True)
-	valuacion=models.FloatField()
+class anio(models.Model):
+	
+	anio=models.DateField()
+	id_descripcion=
 	
 	def __str__(self):
 
